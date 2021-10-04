1ccc560fa132e530c720b20f0d6d200c1b5fa759
rizemon <tan.jia.le.98@gmail.com>
2021-10-04 21:59:48 +0800

    Add Data Class

--- src/main/java/decodex/data/Data.java
+++ src/main/java/decodex/data/Data.java
@@ -1,5 +1,26 @@
-package decodex.data.exception;
+package decodex.data;
 
+/**
+ * The Data class manages the data that is to be encoded or decoded.
+ */
 public class Data {
 
+    private byte[] rawBytes;
+
+    public Data(byte[] rawBytes) {
+        this.rawBytes = rawBytes;
+    }
+
+    public Data(String data) {
+        rawBytes = data.getBytes();
+    }
+
+    public byte[] getRawBytes() {
+        return rawBytes;
+    }
+
+    @Override
+    public String toString() {
+        return new String(rawBytes);
+    }
 }
