554766712e7b5d771fbfc371d83c56c4e3a943f9
Kair0s3 <stianboon@gmail.com>
2021-10-05 20:55:33 +0800

    Updated the JUnit test with respect to the changes to the Base64 Module classes.

--- src/test/java/decodex/modules/Base64EncoderTest.java
+++ src/test/java/decodex/modules/Base64EncoderTest.java
@@ -7,14 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Base64EncoderTest {
 
-    private static final String BASE64_ENCODER_NAME = "base64encode";
-    private static final String BASE64_ENCODER_DESC = "Base64-encodes the data";
-
     @Test
     void run_originalString_base64EncodedData() {
-        Base64Encoder base64Encoder = new Base64Encoder(BASE64_ENCODER_NAME, BASE64_ENCODER_DESC);
+        Base64Encoder base64Encoder = new Base64Encoder();
         String testString = "test";
         String expectedString = "dGVzdA==";
+
         Data originalData = new Data(testString);
         Data encodedData = base64Encoder.run(originalData);
         assertEquals(expectedString, encodedData.toString());
