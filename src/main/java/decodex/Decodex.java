87175691d8c4c2db4241ba1f954b72aa0d5b6e27
Kair0s3 <stianboon@gmail.com>
2021-10-06 13:00:38 +0800

    Made some changes to Decodex.java to incorporate the Parser's methods.

--- src/main/java/decodex/Decodex.java
+++ src/main/java/decodex/Decodex.java
@@ -1,8 +1,16 @@
 package decodex;
 
+import decodex.parser.Parser;
+
 import java.util.Scanner;
 
 public class Decodex {
+
+    /**
+     * Necessary objects to be initialized for Decodex to work properly.
+     */
+    private static Parser parser;
+
     /**
      * Logo to be changed - possibly to a welcome message instead for Decodex.
      */
@@ -18,17 +26,27 @@ public class Decodex {
     private static final String EXIT_COMMAND = "exit";
 
     /**
+     * Initializes the necessary Objects for Decodex.
+     */
+    public static void initDecodex() {
+        parser = new Parser();
+    }
+
+    /**
      * Decodex entry-point for the java.decodex.Decodex application.
      */
     public static void main(String[] args) {
         System.out.println("Hello from\n" + LOGO);
+        initDecodex();
 
         Scanner in = new Scanner(System.in);
         while (true) {
             System.out.print("Decodex > ");
             String userInput = in.nextLine();
+            String command = parser.getCommandType(userInput);
+            String userArgument = parser.getUserArgument(userInput);
 
-            switch (userInput) {
+            switch (command) {
             case EXIT_COMMAND:
                 System.out.print("Goodbye!");
                 System.exit(0);
