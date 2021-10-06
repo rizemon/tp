e700ed1eeb5e116a47f38a90280f689b010420dd
Kair0s3 <stianboon@gmail.com>
2021-10-06 12:52:02 +0800

    Added some of the "base level" methods for parser class.
    1. getCommandType
    2. getUserArgument (Returns a single string argument since our first v1.0 only has one single parameter)

--- src/main/java/decodex/parser/Parser.java
+++ src/main/java/decodex/parser/Parser.java
@@ -1,5 +1,36 @@
 package decodex.parser;
 
+/**
+ * Parses and validates the user input.
+ */
 public class Parser {
 
+    /**
+     * Returns the type of command that user has entered.
+     *
+     * @param userInput The input specified by the user.
+     * @return The type of command.
+     */
+    public String getCommandType(String userInput) {
+        String[] tokens = userInput.split(" ");
+        String commandType = tokens[0];
+        return commandType;
+    }
+
+    /**
+     * Returns the Argument portion of the user input.
+     *
+     * @param userInput The input specified by the user.
+     * @return The argument portion of the user input.
+     */
+    public String getUserArgument(String userInput) {
+        try {
+            String[] tokens = userInput.split(" ");
+            String userArgument = tokens[1];
+            return userArgument;
+        } catch (ArrayIndexOutOfBoundsException err) {
+            // Temporary error handling
+            return "";
+        }
+    }
 }
