5450b83adb0de2caf00fe8c88f2cde05496ddf2c
Sean Phang <e0540763@u.nus.edu>
2021-10-11 16:38:15 +0800

    Resolve suggested changes
    - Remove unused imports

--- src/test/java/decodex/modules/hex/HexDecoderTest.java
+++ src/test/java/decodex/modules/hex/HexDecoderTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import decodex.data.Data;
-import decodex.data.exception.CommandException;
 import decodex.data.exception.ModuleException;
-
 import org.junit.jupiter.api.Test;
 
 class HexDecoderTest {
