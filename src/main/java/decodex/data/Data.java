5a76b5e0a10e048ddbf0d9987dd4625878383eb7
rizemon <tan.jia.le.98@gmail.com>
2021-10-11 15:40:59 +0800

    Add assertions

--- src/main/java/decodex/data/Data.java
+++ src/main/java/decodex/data/Data.java
@@ -1,5 +1,7 @@
 package decodex.data;
 
+import java.util.Arrays;
+
 /**
  * The Data class manages the data that is to be encoded or decoded.
  */
@@ -9,10 +11,12 @@ public class Data {
 
     public Data(byte[] rawBytes) {
         this.rawBytes = rawBytes;
+        assert !Arrays.equals(this.rawBytes, new byte[0]);
     }
 
     public Data(String data) {
         rawBytes = data.getBytes();
+        assert !Arrays.equals(this.rawBytes, new byte[0]);
     }
 
     public byte[] getRawBytes() {
