4d0e4a3d7a9812332894c213878c01ab55f8c756
Kair0s3 <stianboon@gmail.com>
2021-10-18 20:43:07 +0800

    Removed one of the JUnit test for the empty content file, Im thinking I might need to remove all the JUnit for File IO

--- src/test/java/decodex/storage/StorageTest.java
+++ src/test/java/decodex/storage/StorageTest.java
@@ -6,15 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StorageTest {
-
-    @Test
-    void readFromDefaultInputFile_emptyInputFile_emptyString() throws FileException {
-        Storage storage = new Storage();
-        String inputContent = storage.readFromDefaultInputFile();
-
-        assertEquals("", inputContent);
-    }
-
+    
     @Test
     void readFromDefaultInputFile_inputFileWithSomeContent_stringContent() throws FileException {
         Storage storage = new Storage();
