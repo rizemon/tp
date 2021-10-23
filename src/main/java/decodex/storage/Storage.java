13d4e52ea132dee6e0ce938a3e8b8cf9a56425eb
Kair0s3 <stianboon@gmail.com>
2021-10-24 02:34:38 +0800

    Made some of the suggested changes.
    Reverted back to using constants for the directories.
    Changed the .txt to .data instead and its respective naming.

--- src/main/java/decodex/storage/Storage.java
+++ src/main/java/decodex/storage/Storage.java
@@ -14,21 +14,13 @@ import java.time.format.DateTimeFormatter;
 public class Storage {
 
     /**
-     * List of default directories used.
-     * 1. input folder - for reading user input from file.
-     * 2. output folder - for writing processed data into file.
-     * 3. recipe folder - for reading and writing recipe from and to file.
+     * Specifies the default directories for input, output and recipes respectively.
      */
-    private static final String[] DEFAULT_DIRECTORY_LIST = {"input", "output", "recipe"};
+    private static final String DEFAULT_INPUT_DIRECTORY = "input";
+    private static final String DEFAULT_OUTPUT_DIRECTORY = "output";
+    private static final String DEFAULT_RECIPE_DIRECTORY = "recipe";
 
-    /**
-     * The indexes of the corresponding directory in the list.
-     */
-    private static final int INPUT_DIRECTORY_INDEX = 0;
-    private static final int OUTPUT_DIRECTORY_INDEX = 1;
-    private static final int RECIPE_DIRECTORY_INDEX = 2;
-
-    private static final String TEXT_FILE_PREFIX = ".txt";
+    private static final String DATA_FILE_PREFIX = ".data";
 
     private static final String RECIPE_FILENAME_FORMAT = "yyyy-dd-MM__HH.mm.ss";
 
@@ -47,9 +39,9 @@ public class Storage {
      *                     the input file does not exist.
      */
     public byte[] readInputFromFile(String fileName) throws IOException {
-        instantiateDirectoryIfNotExist(DEFAULT_DIRECTORY_LIST[INPUT_DIRECTORY_INDEX]);
+        instantiateDirectoryIfNotExist(DEFAULT_INPUT_DIRECTORY);
 
-        File inputDirectory = new File(DEFAULT_DIRECTORY_LIST[INPUT_DIRECTORY_INDEX]);
+        File inputDirectory = new File(DEFAULT_INPUT_DIRECTORY);
         File inputFile = new File(inputDirectory, fileName);
         Path inputFilePath = inputFile.toPath();
 
@@ -66,9 +58,9 @@ public class Storage {
      *                     the input file does not exist.
      */
     public String readRecipeFromFile(String fileName) throws IOException {
-        instantiateDirectoryIfNotExist(DEFAULT_DIRECTORY_LIST[RECIPE_DIRECTORY_INDEX]);
+        instantiateDirectoryIfNotExist(DEFAULT_RECIPE_DIRECTORY);
 
-        File recipeDirectory = new File(DEFAULT_DIRECTORY_LIST[RECIPE_DIRECTORY_INDEX]);
+        File recipeDirectory = new File(DEFAULT_RECIPE_DIRECTORY);
         File recipeFile = new File(recipeDirectory, fileName);
         Path recipeFilePath = recipeFile.toPath();
 
@@ -103,14 +95,14 @@ public class Storage {
      *                     or when writing to the file.
      */
     public void writeOutputToFile(byte[] outputBytes) throws IOException {
-        instantiateDirectoryIfNotExist(DEFAULT_DIRECTORY_LIST[OUTPUT_DIRECTORY_INDEX]);
+        instantiateDirectoryIfNotExist(DEFAULT_OUTPUT_DIRECTORY);
 
         LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(RECIPE_FILENAME_FORMAT);
         String formattedDateTime = currentDateTime.format(dateTimeFormatter);
-        String newOutputFileName = formattedDateTime + TEXT_FILE_PREFIX;
+        String newOutputFileName = formattedDateTime + DATA_FILE_PREFIX;
 
-        File outputDirectory = new File(DEFAULT_DIRECTORY_LIST[OUTPUT_DIRECTORY_INDEX]);
+        File outputDirectory = new File(DEFAULT_OUTPUT_DIRECTORY);
         File outputFile = new File(outputDirectory, newOutputFileName);
         writeToFile(outputFile, outputBytes);
     }
@@ -124,11 +116,11 @@ public class Storage {
      *                     or when writing to the file.
      */
     public void writeRecipeToFile(String recipeName, byte[] recipeBytes) throws IOException {
-        instantiateDirectoryIfNotExist(DEFAULT_DIRECTORY_LIST[RECIPE_DIRECTORY_INDEX]);
+        instantiateDirectoryIfNotExist(DEFAULT_RECIPE_DIRECTORY);
 
-        String newRecipeFileName = recipeName + TEXT_FILE_PREFIX;
+        String newRecipeFileName = recipeName + DATA_FILE_PREFIX;
 
-        File recipeDirectory = new File(DEFAULT_DIRECTORY_LIST[RECIPE_DIRECTORY_INDEX]);
+        File recipeDirectory = new File(DEFAULT_RECIPE_DIRECTORY);
         File outputRecipeFile = new File(recipeDirectory, newRecipeFileName);
         writeToFile(outputRecipeFile, recipeBytes);
     }
@@ -146,7 +138,7 @@ public class Storage {
             writeFile.createNewFile();
             Path writeFilePath = writeFile.toPath();
             Files.write(writeFilePath, contentBytes);
-        } catch (IOException e) {
+        } catch (IOException err) {
             throw new IOException(ErrorMessages.FILE_WRITE_ERROR_MESSAGE);
         }
     }
@@ -156,11 +148,11 @@ public class Storage {
      */
     public void instantiateDirectoryIfNotExist(String directoryName) throws IOException {
         File outputDirectory = new File(directoryName);
-        boolean isSuccessful;
         if (outputDirectory.exists()) {
             return;
         }
 
+        boolean isSuccessful;
         isSuccessful = outputDirectory.mkdir();
         if (!isSuccessful) {
             throw new IOException(ErrorMessages.DIRECTORY_INSTANTIATION_FAILED_MESSAGE + directoryName);
