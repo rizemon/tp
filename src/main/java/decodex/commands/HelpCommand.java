3b9fa03cfe484c32856be46433087954098fa09c
rizemon <tan.jia.le.98@gmail.com>
2021-10-26 20:58:56 +0800

    Missing comma

--- src/main/java/decodex/commands/HelpCommand.java
+++ src/main/java/decodex/commands/HelpCommand.java
@@ -18,7 +18,7 @@ public class HelpCommand extends Command {
     public static final String COMMAND_HELP_MESSAGE = "Shows list of available commands\n"
             + "Syntax: help";
 
-    private static String[] BASIC_COMMAND_HELP_MESSAGES = {
+    private static final String[] BASIC_COMMAND_HELP_MESSAGES = {
         HelpCommand.COMMAND_HELP_MESSAGE,
         ExitCommand.COMMAND_HELP_MESSAGE,
         InputCommand.COMMAND_HELP_MESSAGE,
@@ -26,9 +26,9 @@ public class HelpCommand extends Command {
         SelectCommand.COMMAND_HELP_MESSAGE
     };
 
-    private static String[] RECIPE_COMMAND_HELP_MESSAGES = {
+    private static final String[] RECIPE_COMMAND_HELP_MESSAGES = {
         RecipeNewCommand.COMMAND_HELP_MESSAGE,
-        RecipeListCommand.COMMAND_HELP_MESSAGE
+        RecipeListCommand.COMMAND_HELP_MESSAGE,
         RecipeSelectCommand.COMMAND_HELP_MESSAGE,
         RecipePushCommand.COMMAND_HELP_MESSAGE,
         RecipePopCommand.COMMAND_HELP_MESSAGE,
